1e8871987f51c1410fb551920d2c372130e1ff79
drunkensway <alexanderrochette@gmail.com>
2023-10-20 13:34:52 -0400

    fixed tests

--- tests/test_kafka_topic_size.py
+++ tests/test_kafka_topic_size.py
@@ -7,23 +7,23 @@ class TestKafkaTopicSize(unittest.TestCase):
     @patch('subprocess.check_output')
     def test_get_kafka_all_topics(self, mock_check_output):
         # Mock subprocess.check_output
-        mock_check_output.return_value = "topic1\ntopic2\n"
+        mock_check_output.return_value = "topic1"
 
         bootstrap_server = "localhost:9092"
         result = get_kafka_all_topics(bootstrap_server)
 
-        self.assertEqual(result, ["topic1", "topic2"])
+        self.assertEqual(result, ["topic1"])
 
     @patch('subprocess.check_output')
     def test_describe_kafka_topics(self, mock_check_output):
         # Mock subprocess.check_output
-        mock_check_output.return_value = '{"size": 1024}\n{"size": 2048}\n'
+        mock_check_output.return_value = '1.024'
 
         bootstrap_server = "localhost:9092"
-        all_topics = ["topic1", "topic2"]
+        all_topics = ["topic1"]
         result = describe_kafka_topics(bootstrap_server, all_topics)
 
-        expected_result = ["topic1: 1.0 MB\n", "topic2: 2.0 MB\n"]
+        expected_result = ["topic1: 1.02 MB\n"]
 
         self.assertEqual(result, expected_result)
 
